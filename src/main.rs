a862c7c26a77314545254a05495ce18f4296d365
voidpumpkin <32368314+voidpumpkin@users.noreply.github.com>
2021-12-27 20:35:14 +0200

    make template use function components

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,7 @@
-use yew::prelude::*;
+mod app;
 
-#[derive(Debug)]
-pub struct Model;
-impl Component for Model {
-    type Message = ();
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
-        unimplemented!()
-    }
-
-    fn changed(&mut self, _ctx: &Context<Self>) -> bool {
-        false
-    }
-
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        html! {
-            <main>
-                <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-                <h1>{ "Hello World!" }</h1>
-                <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
-            </main>
-        }
-    }
-}
+use app::App;
 
 fn main() {
-    yew::start_app::<Model>();
+    yew::start_app::<App>();
 }
