18a6bbe18de130d04490d7a84ae667d43f2b3a3f
Matt "Siyuan" Yan <mattsy1999@gmail.com>
2025-12-08 18:43:58 +0900

    modernize to 0.22.0

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
 use yew::prelude::*;
 
-#[function_component(App)]
-pub fn app() -> Html {
+#[component]
+pub fn App() -> Html {
     html! {
         <main>
             <img class="logo" src="https://yew.rs/img/logo.svg" alt="Yew logo" />
